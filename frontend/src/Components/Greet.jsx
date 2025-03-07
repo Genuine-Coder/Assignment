f108e1ee656ed8df49f166f592d5ae022b2cc71b
Pravesh Kumar <140408257+Genuine-Coder@users.noreply.github.com>
2025-03-07 06:52:49 +0530

    Update Greet.jsx

--- frontend/src/Components/Greet.jsx
+++ frontend/src/Components/Greet.jsx
@@ -16,7 +16,7 @@ function Greet() {
 
     try {
       const response = await axios.get(
-        `http://localhost:8080/api/greet?name=${greetInput}`
+        `http://localhost:8000/api/greet?name=${greetInput}`
       );
       setGreeting(response.data.message);
       setTrack(true);
